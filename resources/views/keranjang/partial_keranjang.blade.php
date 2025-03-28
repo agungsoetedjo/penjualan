84fd79fef5b796e1709b5c197290d4c7a829615d
Agung Soetedjo <fast01archiver@gmail.com>
2025-03-28 19:01:59 +0700

    benerin tombol di keranjang

--- resources/views/keranjang/partial_keranjang.blade.php
+++ resources/views/keranjang/partial_keranjang.blade.php
@@ -1,3 +1,14 @@
+<style>
+    .jumlah-wrapper {
+    height: 30px; /* Kurangi tinggi */
+}
+
+.jumlah-wrapper button,
+.jumlah-wrapper input {
+    height: 28px; /* Sesuaikan dengan wrapper */
+    font-size: 0.9rem; /* Kurangi ukuran font */
+}
+</style>
 @foreach($keranjangItems as $index => $item)
     @if ($index > 0)
         <div class="my-3 border-top"></div> <!-- Divider antar produk -->
@@ -17,9 +28,12 @@
                 @csrf @method('DELETE')
                 <button type="submit" class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></button>
             </form>
-            <button class="btn btn-light btn-sm kurangi" data-id="{{ $item->produk->id }}" {{ $item->jumlah == 1 ? 'disabled' : '' }}>-</button>
-            <input type="text" class="form-control mx-2 text-center" style="width: 50px;" value="{{ $item->jumlah }}" readonly>
-            <button class="btn btn-light btn-sm tambah" data-id="{{ $item->produk->id }}">+</button>
+            <div class="d-flex align-items-center border rounded px-2 py-1 jumlah-wrapper w-100">
+                <button type="button" class="btn btn-sm text-danger border-0 kurangi" data-id="{{ $item->produk->id }}" {{ $item->jumlah == 1 ? 'disabled' : '' }}>-</button>
+                <input type="text" name="jumlah" class="jumlah-input form-control text-center border-0" 
+                value="{{ $item->jumlah }}" readonly style="flex-grow: 1; font-size: 1rem; outline: none; box-shadow: none;">
+                <button type="button" class="btn btn-sm text-success border-0 tambah" data-id="{{ $item->produk->id }}">+</button>
+            </div>
         </div>
     </div>
 @endforeach
